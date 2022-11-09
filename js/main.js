0636034464fcb00b76f9df88df428d84b8175aa5
Daviduo <davide0400@gmail.com>
2022-11-09 19:02:50 +0100

    Orrible css, done 😅

--- js/main.js
+++ js/main.js
@@ -1,18 +1,30 @@
-for (let i = 1; i <= 100; i++){
+// Calcolo numeri e multipli
+const div = document.querySelector("div.number_container")
 
+for (let i = 1; i <= 100; i++){
+    
     if (i % 5 === 0 && i % 3 === 0 ){
         console.log("fizzbuzz");
+        const element = `<div class="number number--fizzbuzz">fizzbuzz<div>`;
+        div.innerHTML += element;
     }
     else if (i % 3 === 0 ){
         console.log("fizz");
+        const element = `<div class="number number--fizz">fizz<div>`;
+        div.innerHTML += element;   
     }
     else if (i % 5 === 0 ){
         console.log("buzz");
+        const element = `<div class="number number--buzz">buzz<div>`;
+        div.innerHTML += element;
     }
     else {
         console.log(i);
+        const element = `<div class="number number--${i}">${i}<div>`;
+        div.innerHTML += element;
     }
 
+    
 
 }
 
